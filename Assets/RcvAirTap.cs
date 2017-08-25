36aad3a0bf202c19152e9e044f941f67c6a0986a
猪膝孝之 <takayuki_0711sosdx@yahoo.co.jp>
2017-08-25 18:58:35 +0900

    initial commit

---
+++ Assets/RcvAirTap.cs
@@ -0,0 +1,32 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RcvAirTap : MonoBehaviour, IInputClickHandler {
+
+    public GameObject original;
+    public TextMesh alphabetTextMesh;
+
+    List<GameObject> list = new List<GameObject>();
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GameObject text = GameObject.Instantiate(original);
+
+        text.transform.position = Camera.main.transform.TransformPoint(0, 0, 1.2f);
+        list.Add(text);
+        Debug.Log("AirTap");
+    }
+
+    // Use this for initialization
+    void Start () {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TypingText.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TypingText : MonoBehaviour
+{
+    TypingSystem ts;
+    public String text;
+    public TextMesh alphabetTextMesh;
+
+    void UpdateText()
+    {
+        alphabetTextMesh.text = "<color=white>" + ts.GetInputedKey() + "</color>" + ts.GetRestKey();
+        //alphabetTextMesh.text = "aiueo";
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        ts = new TypingSystem();
+        ts.SetInputString("");
+        UpdateText();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DownKeyCheck();
+    }
+
+    void DownKeyCheck()
+    {
+        if (Input.anyKeyDown)
+        {
+            string[] keys = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", };
+            foreach (string key in keys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    //処理を書く
+                    // alphabetTextMesh.text = alphabetTextMesh.text + "<color=white>" + key + "</color>";
+                    text = text + key;
+                    alphabetTextMesh.text = "<color=white>" + text + "</color>";
+                    Debug.Log(key);
+                    break;
+                }
+                else if (Input.GetKeyDown("backspace"))
+                {
+                    if (text.ToString() != "")
+                    {
+                        text = text.Substring(0, text.Length - 1);
+                    }
+                    Debug.Log(text);
+                    alphabetTextMesh.text = "<color=white>" + text + "</color>";
+                    break;
+                }
+            }
+        }
+    }
+}
